1c5480db4f6b50a44216208a20e518b1b6b4d455
johanfidellemos <jofileru@yahoo.es>
2022-06-23 10:58:52 -0500

    actualizacion

--- components/Sidemenu.js
+++ components/Sidemenu.js
@@ -4,20 +4,32 @@ import styles from "../styles/Sidemenu.module.css";
 import { FaHome, FaTachometerAlt } from "react-icons/fa";
 import { BsGraphUp } from "react-icons/bs";
 import { useRouter } from "next/router";
+import { Dropdown, DropdownItem, DropdownToggle } from "reactstrap";
+import { useState } from "react";
 
 export default function Sidemenu() {
   const router = useRouter();
-  return (
-    <div className={styles.container_sidemenu}>
-      <FaHome className={styles.icon} onClick={() => router.push("/")}></FaHome>
-      <BsGraphUp
-        className={styles.icon}
-        onClick={() => router.push("/dashboar")}
-      ></BsGraphUp>
-      <FaTachometerAlt
-        className={styles.icon}
-        onClick={() => router.push("/devices")}
-      ></FaTachometerAlt>
-    </div>
-  );
+  function App() {
+    const [dropdown, setDropdown] = useState(false);
+    const abrirCerrarDropdown = () => {
+      setDropdown(!dropdown);
+    };
+
+    return (
+      <div className={styles.container_sidemenu}>
+        <FaHome
+          className={styles.icon}
+          onClick={() => router.push("/")}
+        ></FaHome>
+        <BsGraphUp
+          className={styles.icon}
+          onClick={() => router.push("/dashboar")}
+        ></BsGraphUp>
+        <FaTachometerAlt
+          className={styles.icon}
+          onClick={() => router.push("/devices")}
+        ></FaTachometerAlt>
+      </div>
+    );
+  }
 }
